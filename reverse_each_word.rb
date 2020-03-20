53e3efda0753f43628b9659ab50ecc54dd16d5a9
Steven Kandow <kando1sr@gmail.com>
2020-03-20 15:58:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   string_array.each do |str|
      reverse_array << str.reverse
    end
-  string_array.collect do |str|
+  reverse_array = string_array.collect do |str|
     str.reverse 
   end 
   reverse_array.join(" ")
