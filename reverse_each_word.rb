102ea06a9f0915282173572c8187c9a8bc3fdcb0
Steven Kandow <kando1sr@gmail.com>
2020-03-20 15:39:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
 def reverse_each_word(string)
   string_array = string.split
-  new_string = ""
-  string_array.each {|str| new_string += "#{str.reverse} "}
-  new_string
+  string_array.each {|str| print str.reverse}
 end 
