65c12835774adf774fcf859df02093253e7244de
Steven Kandow <kando1sr@gmail.com>
2020-03-20 15:44:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   string_array = string.split
   reversed_string = ""
-  string_array.each {|str| reversed_string += str.reversed}
+  string_array.each {|str| reversed_string += str.reverse}
 end 
